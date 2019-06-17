10a22c707c8966fbe26169011a552ae13a7efe44
JCRN <clemons.jason@gmail.com>
2019-06-17 15:10:59 -0700

    finished web page, changed link colors

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street \nSomewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -42,54 +42,90 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-// Fix the title
-const title = document.querySelector('title')
-title.textContent = "Great Idea!"
+// *** NAV MENU ***
 
-// Add textContent to nav links
-const navLinks = document.querySelectorAll('nav a')
-navLinks.forEach((link, idx) => {
-  let str = `nav-item-${idx+1}`
-  link.textContent = siteContent.nav[str]
-})
+  // links
+  const navLinks = document.querySelectorAll('nav a')
 
-// insert <br> tags and text into H1
-const h1 = document.querySelector('.cta-text h1')
-h1.style.whiteSpace = 'pre'
-h1.textContent = siteContent.cta['h1']
+  // links text
+  navLinks.forEach((link, idx) => {
+    let str = `nav-item-${idx+1}`
+    link.textContent = siteContent.nav[str]
+  })
 
-// insert button text
-document.querySelector('.cta-text > button').textContent = siteContent.cta['button']
-
-// insert code snippet image
-document.querySelector('#cta-img').src = siteContent.cta['img-src']
-
-// *** Main content ***
-
-// insert top-content
-
-// features
-document.querySelector('.top-content > .text-content > h4').textContent = siteContent['main-content']['features-h4']
-document.querySelector('.top-content > .text-content > p').textContent = siteContent['main-content']['features-content']
-
-// about
-document.querySelector('.top-content > .text-content ~ .text-content > h4').textContent = siteContent['main-content']['about-h4']
-document.querySelector('.top-content > .text-content ~ .text-content > p').textContent = siteContent['main-content']['about-content']
+  // links color
+   navLinks.forEach(link => {
+    link.style.color = 'green'
+  })
 
 
-// middle image
-document.querySelector('.middle-img').src = siteContent['main-content']['middle-img-src']
+  
+// *** CALL TO ACTION ***
 
-// insert bottom content
+  // h1
+  const h1 = document.querySelector('.cta-text h1')
+  h1.style.whiteSpace = 'pre'
+  h1.textContent = siteContent.cta['h1']
 
-// services
-document.querySelector('.bottom-content > .text-content > h4').textContent = siteContent['main-content']['services-h4']
-document.querySelector('.bottom-content > .text-content > p').textContent = siteContent['main-content']['services-content']
+  // button text
+  document.querySelector('.cta-text > button').textContent = siteContent.cta['button']
 
-// product
-document.querySelector('.bottom-content > .text-content ~ .text-content > h4').textContent = siteContent['main-content']['product-h4']
-document.querySelector('.bottom-content > .text-content ~ .text-content > p').textContent = siteContent['main-content']['product-content']
+  // code snippet image
+  document.querySelector('#cta-img').src = siteContent.cta['img-src']
 
-// vision
-document.querySelector('.bottom-content > .text-content ~ .text-content ~ .text-content > h4').textContent = siteContent['main-content']['vision-h4']
-document.querySelector('.bottom-content > .text-content ~ .text-content ~ .text-content > p').textContent = siteContent['main-content']['vision-content']
+
+// *** MAIN CONTENT ***
+
+  // top-content
+
+    // features
+    document.querySelector('.top-content > .text-content > h4').textContent = siteContent['main-content']['features-h4']
+    document.querySelector('.top-content > .text-content > p').textContent = siteContent['main-content']['features-content']
+
+    // about
+    document.querySelector('.top-content > .text-content ~ .text-content > h4').textContent = siteContent['main-content']['about-h4']
+    document.querySelector('.top-content > .text-content ~ .text-content > p').textContent = siteContent['main-content']['about-content']
+
+
+  // middle image
+  document.querySelector('.middle-img').src = siteContent['main-content']['middle-img-src']
+
+  // bottom content
+
+    // services
+    document.querySelector('.bottom-content > .text-content > h4').textContent = siteContent['main-content']['services-h4']
+    document.querySelector('.bottom-content > .text-content > p').textContent = siteContent['main-content']['services-content']
+
+    // product
+    document.querySelector('.bottom-content > .text-content ~ .text-content > h4').textContent = siteContent['main-content']['product-h4']
+    document.querySelector('.bottom-content > .text-content ~ .text-content > p').textContent = siteContent['main-content']['product-content']
+
+    // vision
+    document.querySelector('.bottom-content > .text-content ~ .text-content ~ .text-content > h4').textContent = siteContent['main-content']['vision-h4']
+    document.querySelector('.bottom-content > .text-content ~ .text-content ~ .text-content > p').textContent = siteContent['main-content']['vision-content']
+
+
+// *** CONTACT INFORMATION ***
+
+  // header
+  document.querySelector('.contact h4').textContent = siteContent.contact['contact-h4']
+
+  // address
+  const address = document.querySelector('.contact > p')
+  address.style.whiteSpace = 'pre'
+  address.textContent = siteContent.contact['address']
+
+  // phone number
+  const phone = document.querySelector('.contact > p ~ p')
+  phone.textContent = siteContent.contact['phone']
+
+  // email
+  const email = document.querySelector('.contact > p ~ p ~ p')
+  email.textContent = siteContent.contact['email']
+
+
+// *** FOOTER ***
+
+  // copyright 
+  const copyright = document.querySelector('footer p')
+  copyright.textContent = siteContent.footer['copyright']
