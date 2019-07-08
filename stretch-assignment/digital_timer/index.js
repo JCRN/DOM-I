d1f03332a7d4025fec455d16adc335821799c986
JCRN <clemons.jason@gmail.com>
2019-07-08 15:54:30 -0700

    fixed by placing async in script tag

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,14 @@
+let inetervalID = 0
+const digits = document.querySelectorAll(".digit")
+
+function startTimer() {
+   intervalID = window.setInterval(timer, 10)
+}
+
+function stopTimer() {
+    clearInterval(inetervalID)
+}
+
+function timer() {
+    
+}
